17a3b032e0d3c2491a4cf626cb70a45fe4033c48
Huan2302 <votuonghuan185@gmail.com>
2019-12-12 13:27:56 +0700

    commit profile

--- src/components/ContentComponent.js
+++ src/components/ContentComponent.js
@@ -1,23 +1,57 @@
-import {Breadcrumb, Layout} from "antd";
-import React, {useContext} from "react";
-import {PlayerBarComponent} from "./PlayerBarComponent";
-import {PlayerContext} from "../contexts/playerContext";
+import { Breadcrumb, Layout } from "antd";
+import React, { useContext } from "react";
+import { PlayerBarComponent } from "./PlayerBarComponent";
+import { PlayerContext } from "../contexts/playerContext";
 
-const {Content} = Layout;
+const { Content } = Layout;
 
 export function ContentComponent() {
-    let {audio, state, controls, ref} = useContext(PlayerContext);
+    let { audio, state, controls, ref } = useContext(PlayerContext);
 
-    return <Content style={{margin: "0 16px"}}>
-        <Breadcrumb style={{margin: "16px 0"}}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-        </Breadcrumb>
+    return <Content style={{ margin: "0 0px" }}>
+        
 
-        {/*Content Here*/}
-        <div style={{padding: 24, background: "#fff"}}>{JSON.stringify(state, null, 2)}</div>
+        {/*Content Here*/}      
+        <form>
+            
+            <div className="ms_profile_wrapper" style={{background: "#14182a",margin:0,padding:0}}>
+                <h1>Edit Profile</h1>
+                <div className="ms_profile_box">
+                    <div className="ms_pro_img">
+                        <img src="image/01.jpg" alt="" className="img-fluid" />
+                        <div className="pro_img_overlay">
+                            <i className="fa_icon edit_icon" />
+                        </div>
+                    </div>
+                    <div className="ms_pro_form">
+                        <div className="form-group">
+                            <label>Your Name *</label>
+                            <input type="text" placeholder="Name" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label>Your Email *</label>
+                            <input type="text" placeholder="Name@gmail.com" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label>Your Password *</label>
+                            <input type="password" placeholder="********" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label>Confirm Password *</label>
+                            <input type="password" placeholder="********" className="form-control" />
+                        </div>
+                        <div className="pro-form-btn text-center marger_top15">
+                            <a href="#" className="ms_btn">Lưu</a>
+                            <a href="#" className="ms_btn">Hủy Bỏ</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        
+
         {/*End content*/}
 
-        <PlayerBarComponent/>
+        <PlayerBarComponent />
     </Content>;
 }
